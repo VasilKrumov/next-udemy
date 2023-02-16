adbc5dfcd48aa9c750f0c51f8936a6601ba29833
vasil.krumov <vassil.krumov@gmail.com>
2023-02-16 15:58:09 +0200

    🎉 Routing Project Initial Commit

--- starter-module/pages/blog/[...slug].js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from 'next/router'
-
-export default function BlogPostPage() {
-    const router = useRouter()
-    console.log(router)
-    return <div>Home</div>
-}
--- starter-module/pages/clients/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ClientsPage() {
-    return (
-        <div>
-            <h1>The Clients Page</h1>
-        </div>
-    )
-}

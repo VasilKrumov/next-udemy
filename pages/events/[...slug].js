dcbe017e1776063ba62d48123cdf144105a00a95
vasil.krumov <vassil.krumov@gmail.com>
2023-04-03 17:11:34 +0300

    Added Static Generation

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,26 +1,11 @@
-import { useRouter } from 'next/router'
-import { getFilteredEvents } from '../../dummy-data'
 import { EventList } from '../../components/events/event-list'
 import ResultsTitle from '../../components/events/results-title'
 import Button from '../../components/ui/Button'
 import ErrorAlert from '../../components/ui/error-alert'
+import { getFilteredEvents } from '../../helpers/api-util'
 
-export default function FilteredEventsPage() {
-    const router = useRouter()
-
-    const filterData = router.query.slug
-
-    if (!filterData) {
-        return <p className="center">Loading...</p>
-    }
-
-    const filteredYear = filterData[0]
-    const filteredMonth = filterData[1]
-
-    const numYear = +filteredYear
-    const numMonth = +filteredMonth
-
-    if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+export default function FilteredEventsPage(props) {
+    if (props.hasError) {
         return (
             <ErrorAlert>
                 <p>Invalid filter. Please adjust your values!</p>
@@ -28,12 +13,7 @@ export default function FilteredEventsPage() {
         )
     }
 
-    const filteredEvents = getFilteredEvents({
-        year: numYear,
-        month: numMonth,
-    })
-
-    if (!filteredEvents || filteredEvents.length === 0) {
+    if (!props.events || props.events.length === 0) {
         return (
             <>
                 <ErrorAlert>
@@ -46,12 +26,49 @@ export default function FilteredEventsPage() {
         )
     }
 
-    const date = new Date(numYear, numMonth - 1)
+    const date = new Date(props.date.year, props.date.month - 1)
 
     return (
         <>
             <ResultsTitle date={date} />
-            <EventList items={filteredEvents} />
+            <EventList items={events} />
         </>
     )
+}
+
+export async function getServerSideProps(context) {
+    const { params } = context
+
+    const filterData = params.slug
+
+    const filteredYear = filterData[0]
+    const filteredMonth = filterData[1]
+
+    const numYear = +filteredYear
+    const numMonth = +filteredMonth
+
+    if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+        return {
+            props: { hasError: true },
+            // notFound: true,
+            // redirect: {
+            //   destination: '/error'
+            // }
+        }
+    }
+
+    const filteredEvents = await getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    })
+
+    return {
+        props: {
+            events: filteredEvents,
+            date: {
+                year: numYear,
+                month: numMonth,
+            },
+        },
+    }
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
 import { useRouter } from 'next/router'
-import { getAllEvents } from '../../dummy-data'
 import { EventList } from '../../components/events/event-list'
 import EventsSearch from '../../components/events/events-search'
+import { getAllEvents } from '../../helpers/api-util'
 
-export default function AllEventsPage() {
-    const events = getAllEvents()
+export default function AllEventsPage(props) {
     const router = useRouter()
+    const { events } = props
 
     function findEventsHandler(year, month) {
         const fullPath = `/events/${year}/${month}`
@@ -18,4 +18,15 @@ export default function AllEventsPage() {
             <EventList items={events} />
         </>
     )
+}
+
+export async function getStaticProps() {
+    const events = await getAllEvents()
+
+    return {
+        props: {
+            events,
+        },
+        revalidate: 60,
+    }
 }

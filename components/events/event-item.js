c4821fafd94f7c3cc92fc2187c7f8eeaae427ae3
vasil.krumov <vassil.krumov@gmail.com>
2023-04-04 15:20:10 +0300

    :tada: Module Optimization Complete

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -2,6 +2,7 @@ import AddressIcon from '../icons/address-icon'
 import ArrowRightIcon from '../icons/arrow-right-icon'
 import DateIcon from '../icons/date-icon'
 import Button from '../ui/Button'
+import Image from 'next/image'
 
 import classes from './event-item.module.css'
 
@@ -16,7 +17,7 @@ export const EventItem = ({ title, image, date, location, id }) => {
 
     return (
         <li className={classes.item}>
-            <img src={'/' + image} alt={title} />
+            <Image src={'/' + image} alt={title} width={250} height={160} />
             <div className={classes.content}>
                 <div className={classes.summary}>
                     <h2>{title}</h2>

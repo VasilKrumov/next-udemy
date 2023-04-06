b1f1a6af828fbe55e808ecb6d9fa1bc4f4233c8d
vasil.krumov <vassil.krumov@gmail.com>
2023-04-06 15:19:30 +0300

    :construction: work in progress

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,30 +1,47 @@
-import classes from './newsletter-registration.module.css';
+import classes from './newsletter-registration.module.css'
+import { useRef } from 'react'
 
 function NewsletterRegistration() {
-  function registrationHandler(event) {
-    event.preventDefault();
+    const emailInputRef = useRef()
+    function registrationHandler(event) {
+        event.preventDefault()
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
-  }
+        const enteredEmail = emailInputRef.current.value
 
-  return (
-    <section className={classes.newsletter}>
-      <h2>Sign up to stay updated!</h2>
-      <form onSubmit={registrationHandler}>
-        <div className={classes.control}>
-          <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
-          />
-          <button>Register</button>
-        </div>
-      </form>
-    </section>
-  );
+        fetch('/api/newsletter', {
+            method: 'POST',
+            body: JSON.stringify({ email: enteredEmail }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            if (response.ok) {
+                return response.json()
+            }
+            return response.json().then((data) => {
+                console.log(data)
+                throw new Error(data.message || 'Something went wrong!')
+            })
+        })
+    }
+
+    return (
+        <section className={classes.newsletter}>
+            <h2>Sign up to stay updated!</h2>
+            <form onSubmit={registrationHandler}>
+                <div className={classes.control}>
+                    <input
+                        type="email"
+                        id="email"
+                        placeholder="Your email"
+                        aria-label="Your email"
+                        ref={emailInputRef}
+                    />
+                    <button>Register</button>
+                </div>
+            </form>
+        </section>
+    )
 }
 
-export default NewsletterRegistration;
+export default NewsletterRegistration
